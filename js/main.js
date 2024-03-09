0fa2a3a173e321f67dddbf832524afb9fd6208fa
jordidimass <jordi.dimas@gmail.com>
2024-03-09 15:59:37 -0600

    mobile vesion completed

--- js/main.js
+++ js/main.js
@@ -1,23 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
     var menuIcon = document.querySelector('.menu-icon');
+    var closeIcon = document.querySelector('.close-icon'); 
     var navbarNav = document.querySelector('.navbar-nav');
     var dropdownToggle = document.querySelector('.dropdown-toggle');
     var dropdownMenu = document.querySelector('.dropdown-menu');
 
+    // Event listener to toggle the menu
     menuIcon.addEventListener('click', function () {
-        navbarNav.classList.toggle('show');
+        navbarNav.classList.add('show');
+        closeIcon.style.display = 'block'; 
     });
 
+    // Event listener to close the menu
+    closeIcon.addEventListener('click', function () {
+        navbarNav.classList.remove('show');
+        this.style.display = 'none'; 
+    });
+
+    // Event listener to toggle dropdown items within the navbar
     dropdownToggle.addEventListener('click', function (event) {
         event.preventDefault();
         dropdownMenu.classList.toggle('show');
         dropdownToggle.classList.toggle('active');
     });
 
+    // Fetch dropdown menu items from the JSON file
     fetch('https://cdn.shopify.com/s/files/1/0593/0018/4179/files/menu.json?v=1709760796')
         .then(response => response.json())
         .then(data => {
-            const dropdownMenu = document.querySelector('.dropdown-menu');
             data.forEach(item => {
                 const element = document.createElement('a');
                 element.setAttribute('href', item.url);
